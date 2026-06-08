c7aae2e4736a61f4791bb7c56acb2bcd5376fa78
pgm-stefvanboven <stef.vanboven@student.arteveldehs.be>
2026-06-08 12:54:31 +0200

    updated version 3 of the game with win and lose state

--- Pi-code-versions/halte3_v3.py
+++ Pi-code-versions/halte3_v3.py
@@ -18,29 +18,46 @@ print("Start glucose:", glucose)
 while True:
     pygame.event.pump()
 
-    # Glucose levels drop every second
+    # Elke seconde glucose laten dalen
     if time.time() - latest_drop >= 1:
+
         glucose -= 1
 
         remaining = 60 - int(time.time() - start_time)
+
+        # WIN
+        if remaining <= 0:
+            print("\nGEWONNEN!")
+            print("Je hebt Thomas veilig door deze situatie geholpen.")
+            break
+
+        # VERLIES
+        if glucose <= 30:
+            print("\nGAME OVER")
+            print("Thomas kreeg een ernstige hypo.")
+            break
 
         print("Glucose:", glucose, "| Tijd:", remaining)
 
         latest_drop = time.time()
 
-    # Check buttons
+    # Knoppen controleren
     for i in range(3):
+
         current_status = joystick.get_button(i)
 
         if current_status and not previous_status[i]:
 
+            # Rode knop
             if i == 0:
                 glucose += 10
                 print("Suiker gegeven ->", glucose)
 
+            # Gele knop
             elif i == 1:
                 print("Wachten")
 
+            # Groene knop
             elif i == 2:
                 print("Huidige waarde:", glucose)
 
